60cb8ed0e06978b7644db384bd3356bec405e5fd
penaone <penaone@gmail.com>
2020-10-18 11:03:12 -0700

    server functions

--- server.js
+++ server.js
@@ -56,3 +56,62 @@ function start() {
   });
 };
 
+// functionality 
+function addDptRoleEmp() {
+  inquirer
+    .prompt({
+      name: "add_action",
+      type: "initiallist",
+      message: "What would you like to add?",
+      choices: [
+        "Add departments?",
+        "Add roles?",
+        "Add emplyees?",
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.add_action) {
+      case "Add departments?":
+        addDptRoleEmp();
+        break;
+
+      case "Add roles?":
+        addDptRoleEmp();
+        break;
+
+      }
+    });
+
+};
+
+// adding New components to the Tables
+
+function addDpt() {
+  inquirer
+    .prompt(
+      [{
+        name: "addDptName",
+        type: "input",
+        message: "What is the Name of this new department?"        
+      }]
+    )
+    .then(function(answer) {
+
+      connection.query(
+        "INSERT INTO department SET ?",
+        {
+          name: answer,addDptName,
+
+        },
+
+        function(err) {
+          if (err) throw err;
+          console.log("New Role added successfully!");
+          const table = cTable.getTable([{
+            name: answer.addDptName,
+          }])
+          console.log(table)
+          start();
+        })
+    })
+};
