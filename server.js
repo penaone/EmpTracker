a5622adea7957fd2005febf9f12843eebbe2cde5
penaone <penaone@gmail.com>
2020-10-18 11:00:48 -0700

    Adding functions to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const conTable = require("console.table");
+const { start } = require("repl");
 const connection = mysql.createConnection({
   host: "localhost",
   // Set port; 
@@ -12,4 +13,46 @@ const connection = mysql.createConnection({
   database: "employee_trackerdb"
 });
 
+
+connection.connect(function(err) {
+  if (err) throw err;
+  start();
+
+});
+
+function start() {
+  inquirer
+  .prompt({
+    name: "action",
+    type: "initiallist",
+    message: "What would ou like to do?",
+    choices: [
+      "Add a department/role/employee?",
+      "View existing departments/roles/employees?",
+      "Update employee?",
+      "DELETE?!?"
+    ]
+  })
+
+  .then(function(answer) {
+    switch (answer.action) {
+    case "Add a department/role/employee?":
+      addDptRoleEmp();
+      break;
+
+      case "View existing departments/roles/employees?":
+        viewDptRoleEmp();
+        break;
+
+      case "Update employee?":
+        updateEmp();
+        break;
+
+      case "DELETE?!?":
+        deleteInfo();
+        break;
+
+    }
+  });
+};
 
