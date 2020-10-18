85a1080a2ed5f265d0cab1c4b29c09cfb629fddc
penaone <penaone@gmail.com>
2020-10-18 10:21:51 -0700

    Completed schema file, package.json corrections

--- schema.sql
+++ schema.sql
@@ -1,8 +1,14 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
 Create database employee_trackerDB;
 
-Use employee_trackerDB;
+USE employee_trackerDB;
 
-CREATE TABLE department(
+drop table department;
+drop table role;
+drop table employee;
+
+
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30),
   PRIMARY KEY (id)
@@ -24,6 +30,19 @@ CREATE TABLE employee (
   manager_id INT,
   PRIMARY KEY (id)
   );
+
+  INSERT INTO employee(first_name, last_name, role_id, manager_id)
+  VALUES ("John", "Smith", 1, 1);
+
+  INSERT INTO role(title, salary, department_id)
+  VALUES ("CFO", "95000", 3);
+
+  INSERT INTO department(name)
+  VALUES ("Financial_Services");
+
+  SELECT * FROM department;
+  SELECT * FROM role;  
+  SELECT * FROM employee;
 
 
 
--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const conTable = require("console.table");
 const connection = mysql.createConnection({
   host: "localhost",
   // Set port; 
-  Port: 3305,
+  Port: 3306,
   // Users username
   user: "root",
   // User Password
   password: "password",
   database: "employee_trackerdb"
 });
+
+
